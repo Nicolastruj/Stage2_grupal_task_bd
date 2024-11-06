28edb51c63aa762d7704b2cecc4dffde5e67fa2b
lunahernandez <145288658+lunahernandez@users.noreply.github.com>
2024-11-06 22:25:02 +0000

    Query Engine Structure

---
+++ query-engine/src/main/java/software/searchenginename/implementations/ApiOutput.java
@@ -0,0 +1,10 @@
+package software.searchenginename.implementations;
+
+import software.searchenginename.ports.Output;
+
+public class ApiOutput implements Output {
+    @Override
+    public Pane add(String text) {
+        return null;
+    }
+}
---
+++ query-engine/src/main/java/software/searchenginename/implementations/CsvInvertedIndex.java
@@ -0,0 +1,13 @@
+package software.searchenginename.implementations;
+
+import software.searchenginename.model.Entry;
+import software.searchenginename.model.InvertedIndex;
+
+import java.util.List;
+
+public class CsvInvertedIndex implements InvertedIndex {
+    @Override
+    public List<Entry> get(String word) {
+        return null;
+    }
+}
