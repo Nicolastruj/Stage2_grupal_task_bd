3dcb45e72045b2ea34f809b6c7b69c2d8efccdb5
lunahernandez <145288658+lunahernandez@users.noreply.github.com>
2024-11-13 17:35:53 +0000

    Cleaning indexer module

--- indexer/src/main/java/org/ulpgc/control/IndexerCommand.java
+++ indexer/src/main/java/org/ulpgc/control/IndexerCommand.java
@@ -15,7 +15,8 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class IndexerCommand {
+// TODO review exceptions
+public class IndexerCommand implements Command{
     private final IndexerReader indexerReader;
     private final IndexerStore indexerStore;
 
@@ -24,51 +25,38 @@ public class IndexerCommand {
         this.indexerStore = indexerStore;
     }
 
+    @Override
     public void execute() throws IndexerException {
+        indexLatestBooks();
+        System.out.println("Indexation finished.");
+    }
+
+    private void indexLatestBooks() throws IndexerException {
         Path bookPath = Paths.get(indexerReader.getPath());
-        Path tempTray = Paths.get(System.getProperty("user.dir"), "tempTray");
-        try {
-            if (!Files.exists(tempTray)) {
+        Path tempTrayPath = Paths.get(System.getProperty("user.dir"), "tempTrayPath");
+        moveLatestBooksToTempTray(tempTrayPath, bookPath);
+        indexBooksFrom(tempTrayPath);
+        deleteTempTray(tempTrayPath);
+    }
+
+    private void moveLatestBooksToTempTray(Path tempTray, Path bookPath) throws IndexerException {
+        createIfNotExists(tempTray);
+        copyLatestBooksToTempTray(bookPath, tempTray);
+    }
+
+    private static void createIfNotExists(Path tempTray) throws IndexerException {
+        if (!Files.exists(tempTray)) {
+            try {
                 Files.createDirectories(tempTray);
+            } catch (IOException e) {
+                throw new IndexerException(e.getMessage(), e);
             }
-            copyLatestBooksToTempTray(bookPath, tempTray, 5);
-        } catch (IOException e) {
-            throw new IndexerException("Error al mover los libros al tray", e);
-        }
-        String trayPath = tempTray.toString();
-        List<Book> books = indexerReader.read(trayPath);
-
-        for (Book book : books) {
-            indexerStore.index(book);
-        }
-        try {
-            // Si la tray es un directorio, primero elimina todos los archivos dentro
-            if (Files.isDirectory(tempTray)) {
-                try (Stream<Path> files = Files.walk(tempTray)) {
-                    files.sorted(Comparator.reverseOrder())  // Eliminar primero los archivos más profundos
-                            .forEach(path -> {
-                                try {
-                                    Files.delete(path);
-                                } catch (IOException e) {
-                                    System.err.println("Error deleting file: " + path);
-                                }
-                            });
-                } catch (IOException e) {
-                    throw new IndexerException("No se pudo recorrer el directorio para eliminar los archivos", e);
-                }
-            }
-
-            // Ahora elimina el directorio o archivo vacío
-            Files.deleteIfExists(tempTray);
-            System.out.println("Temporary tray deleted: " + tempTray);
-        } catch (IOException e) {
-            throw new IndexerException("No se pudo eliminar la tray", e);
         }
     }
 
-    private void copyLatestBooksToTempTray(Path bookPath, Path tempTray, int n) throws IOException {
+    private void copyLatestBooksToTempTray(Path bookPath, Path tempTray) throws IndexerException {
         try (Stream<Path> paths = Files.walk(bookPath)) {
-            paths.filter(Files::isRegularFile)  // Filtramos solo archivos (libros)
+            paths.filter(Files::isRegularFile)
                     .sorted((p1, p2) -> {
                         try {
                             return Files.getLastModifiedTime(p2).compareTo(Files.getLastModifiedTime(p1));
@@ -76,18 +64,52 @@ public class IndexerCommand {
                             throw new UncheckedIOException(e);
                         }
                     })
-                    .limit(n)  // Limitamos a los n libros más recientes
+                    .limit(5)
                     .forEach(sourcePath -> {
+                        Path destinationPath = tempTray.resolve(sourcePath.getFileName());
                         try {
-                            // Obtener el nombre del archivo y la ruta destino
-                            Path destinationPath = tempTray.resolve(sourcePath.getFileName());
-
-                            // Copiar el archivo al directorio temporal
                             Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
                         } catch (IOException e) {
-                            e.printStackTrace();
+                            throw new RuntimeException(e);
                         }
+
                     });
+        } catch (IOException e) {
+            throw new IndexerException(e.getMessage(), e);
+        }
+    }
+
+    private void indexBooksFrom(Path tempTrayPath) throws IndexerException {
+        List<Book> books = indexerReader.read(String.valueOf(tempTrayPath));
+
+        for (Book book : books) {
+            indexerStore.index(book);
+        }
+    }
+
+    private static void deleteTempTray(Path tempTrayPath) throws IndexerException {
+        try {
+            deleteFilesFrom(tempTrayPath);
+            Files.deleteIfExists(tempTrayPath);
+        } catch (IOException e) {
+            throw new IndexerException("Failed to delete the temp tray", e);
+        }
+    }
+
+    private static void deleteFilesFrom(Path tempTrayPath) throws IndexerException {
+        if (Files.isDirectory(tempTrayPath)) {
+            try (Stream<Path> files = Files.walk(tempTrayPath)) {
+                files.sorted(Comparator.reverseOrder())
+                        .forEach(path -> {
+                            try {
+                                Files.delete(path);
+                            } catch (IOException e) {
+                                System.err.println("Error deleting file: " + path);
+                            }
+                        });
+            } catch (IOException e) {
+                throw new IndexerException("Failed to walk the directory to delete files", e);
+            }
         }
     }
 }
--- indexer/src/main/java/org/ulpgc/ports/IndexerReader.java
+++ indexer/src/main/java/org/ulpgc/ports/IndexerReader.java
@@ -6,6 +6,7 @@ import org.ulpgc.model.Book;
 import java.util.List;
 
 public interface IndexerReader {
-    public List<Book> read(String path) throws IndexerException;
-    public String getPath() throws IndexerException;
+    List<Book> read(String path) throws IndexerException;
+
+    String getPath() throws IndexerException;
 }
