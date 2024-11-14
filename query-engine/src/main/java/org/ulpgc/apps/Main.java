9bb6ea31fea4a6f26a662eb8f2d839bf5c877ff4
Ivan Anikin <ivan.anikin@outlook.com>
2024-11-14 17:44:43 +0000

    expanded query

--- query-engine/src/main/java/org/ulpgc/apps/Main.java
+++ query-engine/src/main/java/org/ulpgc/apps/Main.java
@@ -7,11 +7,26 @@ import org.ulpgc.implementations.SearchOutput;
 import org.ulpgc.ports.Input;
 import org.ulpgc.ports.Output;
 
+// public class Main {
+//     public static void main(String[] args) throws QueryEngineException{
+//         Input input = new SearchInput();
+//         Output output = new SearchOutput();
+//         SearchEngineCommand controller = new SearchEngineCommand(input, output);
+//         try {
+//             controller.Controller();
+//         } catch (QueryEngineException e) {
+//             throw new QueryEngineException(e.getMessage(), e);
+//         }
+//     }
+// }
+
+import org.ulpgc.control.SearchExpandedCommand;
+
 public class Main {
     public static void main(String[] args) throws QueryEngineException{
         Input input = new SearchInput();
         Output output = new SearchOutput();
-        SearchEngineCommand controller = new SearchEngineCommand(input, output);
+        SearchExpandedCommand controller = new SearchExpandedCommand(input, output);
         try {
             controller.Controller();
         } catch (QueryEngineException e) {
@@ -19,3 +34,4 @@ public class Main {
         }
     }
 }
+
