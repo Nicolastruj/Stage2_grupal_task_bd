8b090d1b712e2340de70a369cb6702420b8f0691
Ivan Anikin <ivan.anikin@outlook.com>
2024-11-14 17:53:59 +0000

    files

---
+++ query-engine/src/main/java/org/ulpgc/apps/Main.java
@@ -0,0 +1,37 @@
+package org.ulpgc.apps;
+
+import org.ulpgc.control.SearchEngineCommand;
+import org.ulpgc.exceptions.QueryEngineException;
+import org.ulpgc.implementations.SearchInput;
+import org.ulpgc.implementations.SearchOutput;
+import org.ulpgc.ports.Input;
+import org.ulpgc.ports.Output;
+
+// public class Main {
+//     public static void main(String[] args) throws QueryEngineException{
+//         Input input = new SearchInput();
+//         Output output = new SearchOutput();
+//         SearchEngineCommand controller = new SearchEngineCommand(input, output);
+//         try {
+//             controller.Controller();
+//         } catch (QueryEngineException e) {
+//             throw new QueryEngineException(e.getMessage(), e);
+//         }
+//     }
+// }
+
+import org.ulpgc.control.SearchExpandedCommand;
+
+public class Main {
+    public static void main(String[] args) throws QueryEngineException{
+        Input input = new SearchInput();
+        Output output = new SearchOutput();
+        SearchExpandedCommand controller = new SearchExpandedCommand(input, output);
+        try {
+            controller.Controller();
+        } catch (QueryEngineException e) {
+            throw new QueryEngineException(e.getMessage(), e);
+        }
+    }
+}
+
