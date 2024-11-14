0d5a223b2fe46ec08cefe8558a7304f58f55e0ef
Ivan Anikin <ivan.anikin@outlook.com>
2024-11-14 17:23:13 +0000

    expanded query

--- query-engine/src/main/java/org/ulpgc/control/SearchExpandedCommand.java
+++ query-engine/src/main/java/org/ulpgc/control/SearchExpandedCommand.java
@@ -22,9 +22,9 @@ public class SearchExpandedCommand implements Command{
         System.out.println("\nWelcome to the Search Engine!");
         System.out.println("If you want to exit the search engine, type 'EXIT'");
 
-        Path bookFolder = Paths.get(System.getProperty("user.dir"), "../BookDatalake");
-        Path indexFolder = Paths.get(System.getProperty("user.dir"), "../InvertedIndex");
-        Path metadataPath = Paths.get(System.getProperty("user.dir"), "../metadata.csv");
+        Path bookFolder = Paths.get(System.getProperty("user.dir"), "BookDatalake");
+        Path indexFolder = Paths.get(System.getProperty("user.dir"), "InvertedIndex");
+        Path metadataPath = Paths.get(System.getProperty("user.dir"), "metadata.csv");
         System.out.println("*_*");
         System.out.println(bookFolder);
         System.out.println(indexFolder);
